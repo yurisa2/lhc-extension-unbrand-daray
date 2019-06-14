1e4d13b057ecda6e333b462831c2a1bb86a2bb93
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-13 23:29:24 -0300

    Demo Footer

--- design/unbrandtheme/tpl/pagelayouts/parts/page_footer.tpl.php
+++ design/unbrandtheme/tpl/pagelayouts/parts/page_footer.tpl.php
@@ -14,14 +14,15 @@ if(isset($_SESSION[lhc_ezcAuth_id])) {
 
 
   $status = json_decode(file_get_contents($ws_url_base.'/wapp-LHC-Bridge/wapp_status.php'));
+  file_get_contents($ws_url_base.'/wapp-LHC-Bridge/demo_hook.php');
 
 echo "<div class=\"col-sm\">";
 
   if($status->online == 1) 
   // echo "<a class=\"btn btn-success\" href = \"../../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_list.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> $status->username Conectado! ".date('d/m/Y H:i:s',$status->time)."</a>";
-  echo "<a class=\"btn btn-success\" href = \"../../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_list.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> $status->username Conectado! </a>";
+  echo "<a class=\"btn btn-success\" href = \"$_SERVER[SCRIPT_NAME]/../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_list.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> $status->username Conectado! </a>";
   else
-  echo "<a class=\"btn btn-danger\" href = \"../../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_list.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> $status->username DESCONECTADO! </a>";
+  echo "<a class=\"btn btn-danger\" href = \"$_SERVER[SCRIPT_NAME]//../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_list.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> $status->username DESCONECTADO! </a>";
   
 
   
@@ -29,8 +30,8 @@ echo "  </div>";
   
 echo "
 <div class=\"pull-right\">
-<a class=\"btn btn-warning\" href = \"../../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_list.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> Abrir Contatos </a>
-<a class=\"btn btn-success\" href = \"../../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_inits_add.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> Iniciar contato </a>
+<a class=\"btn btn-warning\" href = \"$_SERVER[SCRIPT_NAME]/../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_list.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> Abrir Contatos </a>
+<a class=\"btn btn-success\" href = \"$_SERVER[SCRIPT_NAME]/../../wapp-LHC-Bridge/lhc-extensions-bookmarks/output/bookmarks_inits_add.php?user=$_SESSION[lhc_ezcAuth_id]\" target=_blank> Iniciar contato </a>
 
 </div>
 
@@ -40,9 +41,15 @@ echo "
 "; 
 
 
+} else {
+  echo "<a href=# class=\"btn btn-warning\"> USUARIO: demo | SENHA: demo</a>";
+  
 }
+  
+
 
 ?>
+
 <div class="p-1 border-top">
     <p class="float-right small"><a target="_blank" href="http://sa2.com.br">Sa2 &copy; <?php echo date('Y')?></a></p>
     <!-- <p class="small"><a href="<?php echo erLhcoreClassModelChatConfig::fetch('customer_site_url')->current_value?>"><?php // echo htmlspecialchars(erLhcoreClassModelChatConfig::fetch('customer_company_name')->current_value)?></a></p> -->
@@ -50,3 +57,5 @@ echo "
 
 <?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/page_footer_js.tpl.php'));?>
 <?php include_once(erLhcoreClassDesign::designtpl('pagelayouts/parts/page_footer_js_extension_multiinclude.tpl.php'));?>
+
+
